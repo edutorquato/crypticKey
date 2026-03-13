11617acc7e39d8c8dda9eeff15f158484dc9c141
Eduardo <eduardo@homemmaquina.com.br>
2026-03-13 16:24:25 -0300

    ajuste no codigo

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -30,83 +30,17 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
-        $data = $user->data;
-
-        return Inertia::render('Admin/Users/Edit', [
-            'user' => $user,
-            'data' => $data
-        ]);
+        //
     }
 
     public function update(Request $request, User $user)
     {
-        $request->validate([
-            'email' => 'required|email',
-            'person_type' => 'required',
-        ]);
-
-        $user->update([
-            'email' => $request->email,
-        ]);
-
-        $data = $user->data;
-
-        if (!$data) {
-            $data = $user->data()->create([]);
-        }
-
-        $data->person_type = $request->person_type;
-        $data->full_name = $request->full_name;
-        $data->document = $request->document;
-        $data->phone = $request->phone;
-
-        if ($request->hasFile('cnh_file')) {
-            $data->cnh_file = $request->file('cnh_file')->store('documents');
-        }
-
-        if ($request->hasFile('instructor_credential')) {
-            $data->instructor_credential = $request->file('instructor_credential')->store('documents');
-        }
-
-        if ($request->hasFile('criminal_record')) {
-            $data->criminal_record = $request->file('criminal_record')->store('documents');
-        }
-
-        if ($request->hasFile('detran_certificate')) {
-            $data->detran_certificate = $request->file('detran_certificate')->store('documents');
-        }
-
-        if ($request->hasFile('face_photo')) {
-            $data->face_photo = $request->file('face_photo')->store('documents');
-        }
-
-        $data->save();
-
-        return redirect()->route('users.show', $user->id);
-    }
-
-    public function suspend(User $user)
-    {
-        $user->status = 0;
-        $user->save();
-
-        return redirect()->back();
-    }
-
-    public function destroy(User $user)
-    {
-        $user->delete();
-
-        return redirect()->route('admin.users');
+        //
     }
 
     public function show(User $user)
     {
-        $user->load('data');
-
-        return Inertia::render('Admin/Users/Show', [
-            'user' => $user
-        ]);
+        //
     }
 
 }
